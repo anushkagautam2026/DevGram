47be8e3bfd97da3e69356222b2fb4211bc27c25d
ANUSHKA <chinkanu2020@gmail.com>
2025-01-11 22:00:11 +0530

    signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,7 +5,7 @@ export const signup=async(req,res,next)=>{
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || password === '' || email === '') {
-      next(errorHandler(400,'All fields are required'));
+      return next(errorHandler(400,'All fields are required'));
     }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header'
@@ -22,4 +22,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
