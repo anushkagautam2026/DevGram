1fe715022738ab8ede4d0de10e5d7235514b8ff6
ANUSHKA <chinkanu2020@gmail.com>
2025-02-09 19:05:43 +0530

    fixing code issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Home from './pages/Home';
+import Home from '/pages/Home';
 import About from './pages/About';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
